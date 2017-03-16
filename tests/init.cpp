c248c7c4ddeecc7361c4db075e37ec87536639c5
djabri1422 <elmezov99@gmail.com>
2017-03-16 13:28:43 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -6,12 +6,3 @@ SCENARIO("matrix init", "[init]") {
 	REQUIRE(matrix.rows() == 0);
 	REQUIRE(matrix.columns() == 0);
 }
-
-SCENARIO("matrix init", "[init]") {
-	vector_t vector;
-	REQUIRE(vector.size()==0);
-	REQUIRE(vector.capacity() ==0);
-	for(int i=0;i<vector.size();i++){
-		REQUIRE(matrix[i][j]==0)
-	}
-}
