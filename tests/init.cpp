64bd1f3ca3e995b3048492677e03ebf35407f83c
djabri1422 <elmezov99@gmail.com>
2017-03-18 22:26:04 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -3,23 +3,7 @@
                                     
 SCENARIO("matrix init", "[init]") {
 	matrix_t matrix;
-	REQUIRE(matrix.rows() == 0);
-	REQUIRE(matrix.columns() == 0);
-}
-                                   
-SCENARIO("matrix init", "[init]") {
-	matrix_t matrix;
-	REQUIRE(matrix.size()==0);
-	REQUIRE(matrix.capacity() ==0);
-	for(int i=0;i<matrix.size();i++){
-		REQUIRE(matrix[i][j]==0)
-	}
+	REQUIRE(matrix.count_1() == 0);
+	REQUIRE(matrix.count_2() == 0);
 }
 
-SCENARIO("matrix sum","[inti]"){
-	matrix_t a, b, e;
-	std::ifstream("A.txt") >> a;
-	std::ifstream("B.txt") >> b;
-	std::ifstream("A+B.txt") >> e;
-	REQUIRE(a+b == e);
-}
