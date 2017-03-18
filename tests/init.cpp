1814ee41a436969d5e7fe64c0b531f6ac31bc4f8
djabri1422 <elmezov99@gmail.com>
2017-03-18 22:42:08 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,7 +1,7 @@
 #include <matrix.hpp>
 #include <catch.hpp>
                                     
-SCENARIO("matrix init", "[init]") {
+SCENARIO("tmatrix init", "[init]") {
 	tmatrix matrix;
 	REQUIRE(matrix.count_1() == 0);
 	REQUIRE(matrix.count_2() == 0);
