556481d11233e253e03b39eb25435763df754020
djabri1422 <elmezov99@gmail.com>
2017-03-19 13:49:35 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -13,12 +13,3 @@ SCENARIO("params init", "[params init]") {
 	REQUIRE(matrix.rows() == 5);
 	REQUIRE(matrix.columns() == 5);
 }
-
-SCENARIO("sum init","[sum init]")
-{
-	tmatrix a, b, e;
-	std::ifstream("A.txt") >> a;
-	std::ifstream("B.txt") >> b;
-	std::ifstream("A+B.txt") >> e;
-	REQUIRE(a+b == e);
-}
